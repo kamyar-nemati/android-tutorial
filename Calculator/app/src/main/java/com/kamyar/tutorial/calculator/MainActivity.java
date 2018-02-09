434e46aeb7c26cb0b27da719eba7efa3f5693b36
kamyar-nemati <kamyarnemati@gmail.com>
2018-02-09 10:52:09 +0800

    Interface and logic implemented

--- Calculator/app/src/main/java/com/kamyar/tutorial/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/kamyar/tutorial/calculator/MainActivity.java
@@ -2,12 +2,95 @@ package com.kamyar.tutorial.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputA;
+    EditText inputB;
+    EditText result;
+    Button btnAdd;
+    Button btnSub;
+    Button btnMul;
+    Button btnDiv;
+    Button btnClean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputA = (EditText) findViewById(R.id.inputA);
+        inputB = (EditText) findViewById(R.id.inputB);
+        result = (EditText) findViewById(R.id.result);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMul = (Button) findViewById(R.id.btnMul);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+        btnClean = (Button) findViewById(R.id.btnClear);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(inputA.getText().toString());
+                double b = Double.parseDouble(inputB.getText().toString());
+
+                double r = a + b;
+                result.setText(String.valueOf(r));
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(inputA.getText().toString());
+                double b = Double.parseDouble(inputB.getText().toString());
+
+                double r = a - b;
+                result.setText(String.valueOf(r));
+            }
+        });
+
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(inputA.getText().toString());
+                double b = Double.parseDouble(inputB.getText().toString());
+
+                double r = a * b;
+                result.setText(String.valueOf(r));
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(inputA.getText().toString());
+                double b = Double.parseDouble(inputB.getText().toString());
+
+                double r = a / b;
+                result.setText(String.valueOf(r));
+            }
+        });
+
+        btnClean.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return clearInputs(v);
+            }
+        });
+    }
+
+    public void clearResult(View v) {
+        result.setText("");
+    }
+
+    private boolean clearInputs(View v) {
+        inputA.setText("");
+        inputB.setText("");
+        result.setText("");
+        return true;
     }
 }
